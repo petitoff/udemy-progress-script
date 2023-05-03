cc842e95dd4c0dd12a930ad0a334456529cdeaad
Błażej Domagała <petitoff@outlook.com>
2023-05-03 19:33:11 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const expandSections = () => {
+  const sectionTogglers = document.querySelectorAll(
+    ".ud-accordion-panel-toggler"
+  );
+  sectionTogglers.forEach((toggler) => {
+    toggler.click();
+  });
+};
+
+const markVideosAsWatched = () => {
+  const progressToggles = document.querySelectorAll(
+    '.curriculum-item-link--progress-toggle--1CMcg input[type="checkbox"]'
+  );
+  progressToggles.forEach((toggle) => {
+    if (!toggle.checked) {
+      toggle.click();
+    }
+  });
+};
+
+expandSections();
+setTimeout(markVideosAsWatched, 2000);
